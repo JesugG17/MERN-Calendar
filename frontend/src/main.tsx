aad015c3ac761317d87e2d86f9b236913788bf29
Jesus Gastelum <gastelumtec21@gmail.com>
2023-07-14 23:53:38 -0700

    router configured

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { CalendarApp } from './CalendarApp.tsx';
 
+
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <CalendarApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )
